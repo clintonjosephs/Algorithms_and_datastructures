f08b7f35ee0a0c74c433846d74ca5d3203558ab0
clintonjosephs <clintonjosephs26@gmail.com>
2022-06-29 08:42:58 +0100

    solved algorithm challenge

---
+++ js/Positive-number-check.js
@@ -0,0 +1,37 @@
+/*
+Write a function:
+
+function solution(A);
+
+that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
+
+For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+
+Given A = [1, 2, 3], the function should return 4.
+
+Given A = [−1, −3], the function should return 1.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [1..100,000];
+each element of array A is an integer within the range [−1,000,000..1,000,000].
+*/
+
+
+let b = 1;
+
+let newArray = A.sort((a, b) => b < a);
+newArray = [...new Set(newArray)];
+
+for (let i = 0; i < newArray.length; i++) {
+    if (newArray[i] !== b) {
+        return b;
+    } 
+    b++;
+}
+
+if (newArray[newArray.length - 1] <= 0 ) {
+    return 1;
+}
+
+return newArray[newArray.length - 1] + 1;
